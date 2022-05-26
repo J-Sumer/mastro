0ee6b224dabba9024fa44338833f58988f1da2cf
Jyothi Sumer <mjyothisumer@gmail.com>
2022-05-25 21:47:35 -0400

    Register page

--- components/Layout.js
+++ components/Layout.js
@@ -16,6 +16,7 @@ const Layout = ({ children }) => {
             <Head>
                 <title>Mastro</title>
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous" />
+                <link href='/static/css/styles.css' rel='stylesheet' />
             </Head>
         )
     }
--- pages/register.js
+++ pages/register.js
@@ -1,7 +1,67 @@
 import Layout from '../components/Layout.js'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
 
 const Register = () => {
-    return <Layout>Register!</Layout>
+
+    const router = useRouter()
+
+    const [state, setState] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: "",
+        success: "",
+        buttonText: "Register"
+    })
+
+    const handleChange = (name) => (event) => {
+        setState({
+            ...state,
+            [name]: event.target.value,
+            error: ""
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        router.push("/")
+    }
+
+    const registerForm = () => {
+
+        const { name, email, password, error, success, buttonText } = state;
+
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className='form-group mb-3'>
+                    <label htmlFor="name" className="form-label">Full Name</label>
+                    <input onChange={handleChange('name')} type="text" className="form-control shadow-sm" id="name" aria-describedby="nameHelp" value={name} />
+                </div>
+
+                <div className='form-group mb-3'>
+                    <label htmlFor="email" className="form-label">Email address</label>
+                    <input onChange={handleChange('email')} type="text" className="form-control shadow-sm" id="email" aria-describedby="emailHelp" value={email} />
+                    <div id="email" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+
+                <div className='form-group mb-3'>
+                    <label htmlFor="password" className="form-label">Password</label>
+                    <input onChange={handleChange('password')} type="password" className="form-control shadow-sm" id="password" aria-describedby="passwordHelp" value={password} />
+                </div>
+
+                <div className='form-group'>
+                    <button className='btn btn-outline-dark shadow-none'>{buttonText}</button>
+                </div>
+            </form>
+        )
+    }
+
+    return <Layout>
+        <h1>Register!</h1>
+        <br />
+        {registerForm()}
+    </Layout>
 }
 
 export default Register
